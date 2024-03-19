d38fe7ec5f1fd1cf2980f6b975117fcaa2bb7b17
sweety-shree <152768615+sweety-shree@users.noreply.github.com>
2024-03-19 15:55:06 +0530

    bash.sh

--- bash.sh
+++ bash.sh
@@ -1,6 +1,6 @@
 sudo yum install git -y
-git clone https://github.com/sweety-shree/fish.git
+git clone https://github.com/sweety-shree/car-prediction.git
 sudo yum install -y python3-pip
-cd fish
+cd car-prediction
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
