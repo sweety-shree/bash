c928571895477c4a46e7e305f172ea27a9c1e060
sweety-shree <152768615+sweety-shree@users.noreply.github.com>
2024-03-20 16:33:05 +0530

    bash.sh

--- bash.sh
+++ bash.sh
@@ -1,6 +1,6 @@
 sudo yum install git -y
-git clone https://github.com/sweety-shree/fish.git
+git clone https://github.com/sweety-shree/Portfolio.git
 sudo yum install -y python3-pip
-cd fish
+cd Portfolio
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
